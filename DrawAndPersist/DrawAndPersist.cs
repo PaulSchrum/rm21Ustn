394285baf3fde50d3dfffd9bbd7f33c7b2641282
Paul Schrum <paul.schrum@gmail.com>
2013-05-27 17:49:34 -0400

    WIP Impl Draw Ribbon in 2D Plan View; added drawing of Arcs.

--- DrawAndPersist/DrawAndPersist.cs
+++ DrawAndPersist/DrawAndPersist.cs
@@ -13,13 +13,33 @@ namespace rm21Ustn.DrawAndPersist
    class DrawAndPersist : rm2Ubase, IPersistantDrawer
    {
 
+      // todo: DrawAndPersist technical debt: Does not yet assign RM21 named group stuff.
+      // todo: DrawAndPersist technical debt: Does not yet place cross slope elements.
+
       public void PlaceArc(rm21HorArc arc, 
          ptsPoint startPoint, StationOffsetElevation startSOE, 
          ptsPoint endPoint, StationOffsetElevation endSOE)
       {
-         int i = 9;
-      }
+         Point3d uPtStart = rm2Upoint.CreateUstnPoint(startPoint);
+         Point3d uPtEnd = rm2Upoint.CreateUstnPoint(endPoint);
+         Point3d uCenterPt = rm2Upoint.CreateUstnPoint(arc.ArcCenterPt);
 
+         ArcElement newArc = null;
+         if (arc.Deflection.getAsRadians() < 0.0)
+         {
+             newArc =
+               app.CreateArcElement1(null, ref uPtStart, ref uCenterPt, ref uPtEnd);
+         }
+         else
+         {
+            newArc =
+               app.CreateArcElement1(null, ref uPtEnd, ref uCenterPt, ref uPtStart);
+            newArc.Reverse();
+         }
+
+         processNewElement(newArc, arc);
+      }
+      
       public void PlaceLine(rm21HorLineSegment lineSegment,
          ptsPoint startPoint, StationOffsetElevation startSOE,
          ptsPoint endPoint, StationOffsetElevation endSOE)
@@ -30,8 +50,16 @@ namespace rm21Ustn.DrawAndPersist
          LineElement newLine = app.CreateLineElement2(null,
             ref uPtStart, ref uPtEnd);
 
-         activeModelRef.AddElement(newLine);
+         processNewElement(newLine, lineSegment);
       }
 
+      private void processNewElement(Element newElement, HorizontalAlignmentBase rm21GoverningElement)
+      {
+         activeModelRef.AddElement(newElement);
+         newElement.IsLocked = true;
+         newElement.Rewrite();
+
+         //rm21GoverningElement.
+      }
    }
 }
